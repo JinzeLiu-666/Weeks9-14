4a755d2ca4ad248d500ef570a3c218f1833b490a
TFboyAlan <liujinze1912@gmail.com>
2025-04-07 17:09:02 -0400

    Simplified methods using arrays

--- Assets/Assignment System/Scripts/ChangeBackground.cs
+++ Assets/Assignment System/Scripts/ChangeBackground.cs
@@ -6,53 +6,14 @@ using UnityEngine;
 public class ChangeBackground : MonoBehaviour
 {
     public SpriteRenderer backgroundRenderer;
+    public Sprite[] backgrounds;
 
-    public Sprite background1;
-    public Sprite background2;
-    public Sprite background3;
-    public Sprite background4;
-    public Sprite background5;
-    public Sprite background6;
+    private int currentIndex = 0;
 
-    private int currentIndex = 1;
-
+    // Change the image by increasing the number when the UnityEvent's click event is triggered.
     public void NextBackground()
     {
-        // Change the image by increasing the number when the UnityEvent's click event is triggered.
-        if (currentIndex == 0)
-        {
-            backgroundRenderer.sprite = background1;
-            currentIndex = 1;
-        }
-        else if (currentIndex == 1)
-        {
-            backgroundRenderer.sprite = background2;
-            currentIndex = 2;
-        }
-        else if (currentIndex == 2)
-        {
-            backgroundRenderer.sprite = background3;
-            currentIndex = 3;
-        }
-        else if (currentIndex == 3)
-        {
-            backgroundRenderer.sprite = background4;
-            currentIndex = 4;
-        }
-        else if (currentIndex == 4)
-        {
-            backgroundRenderer.sprite = background2;
-            currentIndex = 5;
-        }
-        else if (currentIndex == 5)
-        {
-            backgroundRenderer.sprite = background3;
-            currentIndex = 6;
-        }
-        else if (currentIndex == 6)
-        {
-            backgroundRenderer.sprite = background4;
-            currentIndex = 0;
-        }
+        currentIndex = (currentIndex + 1) % backgrounds.Length;
+        backgroundRenderer.sprite = backgrounds[currentIndex];
     }
 }
